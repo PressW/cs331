e79f9b000c15ea6392bfb492ed58212bae3ca658
PressW <gullysquad02@gmail.com>
2017-04-20 04:05:15 -0700

    added is_goal_state() state_to_string() and bfs() implementations

--- program_1/program_1.py
+++ program_1/program_1.py
@@ -3,6 +3,7 @@ import numpy as np
 import queue
 
 
+
 q = queue.Queue()
 
 
@@ -15,69 +16,65 @@ def set_state(filename):
 
 
 
-def find_successors(state):
-    valid_successors = []
-    
+def find_successors(state, fringe, explored):
     # 1 missionary in the boat
     m1 = {"missionaries":  {"left": 1, "right": -1},
           "cannibals":     {"left": 0, "right": 0},
           "boat":          {"left": 1, "right": -1}}
-    m1_state = state.add(pd.DataFrame(m1))
-    if (valid_state(m1_state)):
-        valid_successors.append(m1_state)
-        q.put(m1_state)
-#    print("\n1 missionary in the boat\n", m1_state)
+    if state.get_value("left", "boat") == 1:
+        m1_state = state.add(-1 * pd.DataFrame(m1))
+    else:
+        m1_state = state.add(pd.DataFrame(m1))
+    if (valid_state(m1_state)) and (state_to_string(m1_state) not in explored):
+        fringe.put(m1_state)
 
     # 2 missionaries in the boat
     m2 = {"missionaries":  {"left": 2, "right": -2},
           "cannibals":     {"left": 0, "right": 0},
           "boat":          {"left": 1, "right": -1}}
-    m2_state = state.add(pd.DataFrame(m2))
-    if (valid_state(m2_state)):
-        valid_successors.append(m2_state)
-        q.put(m2_state)
-#    print("\n2 missionaries in the boat\n", m2_state)
+    if state.get_value("left", "boat") == 1:
+        m2_state = state.add(-1 * pd.DataFrame(m2))
+    else:
+        m2_state = state.add(pd.DataFrame(m2))
+    if (valid_state(m2_state)) and (state_to_string(m2_state) not in explored):
+        fringe.put(m2_state)
 
     # 1 cannibal in the boat
     c1 = {"missionaries":  {"left": 0, "right": 0},
           "cannibals":     {"left": 1, "right": -1},
           "boat":          {"left": 1, "right": -1}}
-    c1_state = state.add(pd.DataFrame(c1))
-    if (valid_state(c1_state)):
-        valid_successors.append(c1_state)
-        q.put(c1_state)
-#    print("\n1 cannibal in the boat\n", c1_state)
+    if state.get_value("left", "boat") == 1:
+        c1_state = state.add(-1 * pd.DataFrame(c1))
+    else:
+        c1_state = state.add(pd.DataFrame(c1))
+    if (valid_state(c1_state)) and (state_to_string(c1_state) not in explored):
+        fringe.put(c1_state)
 
     # 1 cannibal, 1 missionary in the boat
     m1c1 = {"missionaries":  {"left": 1, "right": -1},
           "cannibals":     {"left": 1, "right": -1},
           "boat":          {"left": 1, "right": -1}}
-    m1c1_state = state.add(pd.DataFrame(m1c1))
-    if (valid_state(m1c1_state)):
-        valid_successors.append(m1c1_state)
-        q.put(m1c1_state)
-#    print("\n1 missionary, 1 cannibal in the boat\n", m1c1_state)
+    if state.get_value("left", "boat") == 1:
+        m1c1_state = state.add(-1 * pd.DataFrame(m1c1))
+    else:
+        m1c1_state = state.add(pd.DataFrame(m1c1))
+    if (valid_state(m1c1_state)) and (state_to_string(m1c1_state) not in explored):
+        fringe.put(m1c1_state)
 
     # 2 cannibals in the boat
     c2 = {"missionaries":  {"left": 0, "right": 0},
           "cannibals":     {"left": 2, "right": -2},
           "boat":          {"left": 1, "right": -1}}
-    c2_state = state.add(pd.DataFrame(c2))
-    if (valid_state(c2_state)):
-        valid_successors.append(c2_state)
-        q.put(c2_state)
-#    print("\n2 cannibals in the boat\n", c2_state)
-
-    return valid_successors
+    if state.get_value("left", "boat") == 1:
+        c2_state = state.add(-1 * pd.DataFrame(c2))
+    else:
+        c2_state = state.add(pd.DataFrame(c2))
+    if (valid_state(c2_state)) and (state_to_string(c2_state) not in explored):
+        fringe.put(c2_state)
 
 
 
 def valid_state(state):
-#    print("\nmissionaries right = " , state.get_value("right", "missionaries"))
-#    print("cannibals right = "     , state.get_value("right", "cannibals"))
-#    print("missionaries left = "  , state.get_value("left",  "missionaries"))
-#    print("cannibals left = "      , state.get_value("left",  "cannibals"))
-
     valid = True
     if state.get_value("right", "missionaries") < 0 or state.get_value("left", "missionaries") < 0 or \
        state.get_value("right", "cannibals") < 0    or state.get_value("left", "cannibals") < 0 or \
@@ -89,21 +86,61 @@ def valid_state(state):
     if state.get_value("left", "missionaries") != 0:
         if state.get_value("left", "missionaries") < state.get_value("left", "cannibals"):
             valid = False
-#    print("Valid: ", valid)
     return valid
 
 
+def is_goal_state(state, goal_file):
+    goal_state = set_state(goal_file)
+    if state.get_value("left", "missionaries")  != goal_state.get_value("left", "missionaries"):  return False
+    if state.get_value("left", "cannibals")     != goal_state.get_value("left", "cannibals"):     return False
+    if state.get_value("left", "boat")          != goal_state.get_value("left", "boat"):          return False
+    if state.get_value("right", "missionaries") != goal_state.get_value("right", "missionaries"): return False
+    if state.get_value("right", "cannibals")    != goal_state.get_value("right", "cannibals"):    return False
+    if state.get_value("right", "boat")         != goal_state.get_value("right", "boat"):         return False
+    return True
 
-start_state = set_state("test_start1.txt")
-successors = find_successors(start_state)
-print("\n", successors, "\n")
-valid_state(start_df)
-while not q.empty():
-    print(q.get())
 
-print("\n", start_state, "\n")
-goal_state = set_state("test_goal1.txt")
-print("\n", goal_state, "\n")
 
-# print access index in dataframe
-# print(goal_state.get_value("left","missionaries"), "\n")
+def state_to_string(state):
+    return str(state.get_value("left", "missionaries"))  + str(state.get_value("left", "cannibals"))  + str(state.get_value("left", "boat")) + \
+           str(state.get_value("right", "missionaries")) + str(state.get_value("right", "cannibals")) + str(state.get_value("right", "boat"))
+
+
+
+def bfs(start, goal_file):
+    print("\nSTART STATE\n", start, "\n")
+    fringe = queue.Queue()
+    fringe.put(start)
+    explored = {}
+    explored[state_to_string(start)] = start
+    while not fringe.empty():
+        cur_state = fringe.get()
+        find_successors(cur_state, fringe, explored)
+        explored[state_to_string(cur_state)] = cur_state
+        if is_goal_state(cur_state, goal_file): return explored
+
+
+
+def print_queue(q, q_name):
+    if q.empty():
+        print("\n", q_name, "is empty\n")
+    else:
+        print("\n", q_name, "contents:\n")
+        while not q.empty():
+            print(q.get())
+        print("\n")
+
+
+
+def print_dict(explored):
+    print("\nExplored contents:\n")
+    for key in explored:
+        # print("\n", key)s
+        print(explored[key], "\n")
+
+
+
+if __name__ == "__main__":
+    start_state = set_state("test_start1.txt")
+    explored = bfs(start_state, "test_goal1.txt")
+    print_dict(explored)
